35e9534c64500601f1bec7e2e299ed7d868bb5f7
parkjaehee <jaehee3505@naver.com>
2023-12-07 18:56:48 +0900

    Sign Up Screen

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,146 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:tictok_app/constants/Gaps.dart';
 import 'package:tictok_app/constants/Sizes.dart';
+import 'package:tictok_app/screens/login.dart';
 
-// class SignUpScreen extends StatelessWidget {
-//   const SignUpScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Padding(
-//           padding: EdgeInsets.symmetric(
-//             horizontal: Sizes.size20,
-//           ),
-//           child: Column(
-//             children: [
-//               Gaps.v48,
-//               Text(
-//                 'Sign up for TicTok',
-//                 style: TextStyle(
-//                   fontSize: Sizes.size20,
-//                   fontWeight: FontWeight.w600,
-//                 ),
-//               ),
-//               Gaps.v10,
-//               Text(
-//                 'Create a profile, follow other accounts, make your won videos, and more.',
-//                 style: TextStyle(
-//                   fontSize: Sizes.size14,
-//                   color: Colors.grey,
-//                 ),
-//                 textAlign: TextAlign.center,
-//               )
-//             ],
-//           ),
-//         ),
-//       ),
-//       bottomNavigationBar: BottomAppBar(
-//         color: Colors.red,
-//         elevation: 2,
-//         child: Padding(
-//           padding: const EdgeInsets.symmetric(
-//             vertical: Sizes.size2,
-//           ),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               const Text(
-//                 'Already have an account?',
-//                 style: TextStyle(
-//                   fontSize: Sizes.size16,
-//                 ),
-//               ),
-//               Gaps.h5,
-//               GestureDetector(
-//                 onTap: () {},
-//                 child: Text(
-//                   'Log in',
-//                   style: TextStyle(
-//                     fontSize: Sizes.size16,
-//                     fontWeight: FontWeight.w600,
-//                     color: Theme.of(context).primaryColor,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   void onLoginTap(BuildContext context) {
-    // Navigator.of(context).push(
-    //   MaterialPageRoute(
-    //     builder: (context) => const LoginScreen(),
-    //   ),
-    // );
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => LoginScreen(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Sizes.size40,
-          ),
-          child: Column(
-            children: const [
-              Gaps.v80,
-              Text(
-                "Sign up for TikTok",
-                style: TextStyle(
-                  fontSize: Sizes.size24,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              Gaps.v20,
-              Text(
-                "Create a profile, follow other accounts, make your own videos, and more.",
-                style: TextStyle(
-                  fontSize: Sizes.size16,
-                  color: Colors.black45,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              Gaps.v40,
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.red,
-        elevation: 2,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Already have an account?',
-              style: TextStyle(
-                fontSize: Sizes.size16,
+    return SafeArea(
+      child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Gaps.v52,
+                  Text(
+                    'Sign up for TicTok',
+                    style: TextStyle(
+                      fontSize: Sizes.size24,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Gaps.v24,
+                  Text(
+                    'Create a profile, follow other accounts, make your own videos, and more.',
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      color: Colors.black54,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Gaps.v40,
+                ],
               ),
             ),
-            Gaps.h5,
-            GestureDetector(
-              onTap: () => onLoginTap(context),
-              child: Text(
-                'Log in',
-                style: TextStyle(
-                  fontSize: Sizes.size16,
-                  fontWeight: FontWeight.w600,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+              color: Colors.grey.shade100,
+              elevation: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: TextStyle(
+                      fontSize: Sizes.size14,
+                    ),
+                  ),
+                  Gaps.h5,
+                  GestureDetector(
+                    onTap: () => onLoginTap(context),
+                    child: Text(
+                      'Log in',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: Sizes.size14,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  )
+                ],
+              ))),
     );
   }
 }
