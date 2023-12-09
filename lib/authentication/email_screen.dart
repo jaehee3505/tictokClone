785dfcfabdaa2f0645a11e31a25f537bda1bf6de
parkjaehee <jaehee3505@naver.com>
2023-12-09 16:18:36 +0900

    email screen

--- lib/authentication/email_screen.dart
+++ lib/authentication/email_screen.dart
@@ -24,59 +24,73 @@ class _EmailScreenState extends State<EmailScreen> {
     });
   }
 
+  String? isEmailValid() {
+    if (username.isEmpty) return null;
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(username)) {
+      return 'Invalid Email';
+    }
+    return null;
+  }
+
   @override
   void dispose() {
     usernameController.dispose();
     super.dispose();
   }
 
-  void onEmailTap() {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => EmailScreen()));
+  void onScaffoldTap() {
+    FocusScope.of(context).unfocus();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Sign Up',
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: Sizes.size40,
-        ),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Gaps.v20,
-          Text('What is your email?',
-              style: TextStyle(
-                  fontSize: Sizes.size24,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black)),
-          Gaps.v10,
-          TextField(
-            controller: usernameController,
-            cursorColor: Theme.of(context).primaryColor,
-            decoration: InputDecoration(
-                hintText: 'Email',
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
-                  ),
-                )),
+    return GestureDetector(
+      onTap: onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Sign Up',
           ),
-          Gaps.v20,
-          FormButton(
-            onTap: onEmailTap,
-            isDisable: username.isEmpty,
-          )
-        ]),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Gaps.v20,
+            Text('What is your email?',
+                style: TextStyle(
+                    fontSize: Sizes.size24,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black)),
+            Gaps.v10,
+            TextField(
+              controller: usernameController,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: InputDecoration(
+                  errorText: isEmailValid(),
+                  hintText: 'Email',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  )),
+            ),
+            Gaps.v20,
+            FormButton(
+              onTap: () {},
+              isDisable: username.isEmpty,
+            )
+          ]),
+        ),
       ),
     );
   }
--- lib/authentication/username_screen.dart
+++ lib/authentication/username_screen.dart
@@ -31,61 +31,76 @@ class _EmailScreenState extends State<UsernameScreen> {
     super.dispose();
   }
 
+  void onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   void onEmailTap() {
     if (username.isEmpty) return;
+
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => EmailScreen()));
   }
 
+  String? isUsernameValid() {
+    if (username.isEmpty) return 'Invalid Username';
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Sign Up',
+    return GestureDetector(
+      onTap: onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Sign Up',
+          ),
         ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: Sizes.size40,
-        ),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Gaps.v20,
-          Text('Create username',
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Gaps.v20,
+            Text('Create username',
+                style: TextStyle(
+                    fontSize: Sizes.size24,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black)),
+            Gaps.v5,
+            Text(
+              'You can always change this later',
               style: TextStyle(
-                  fontSize: Sizes.size24,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black)),
-          Gaps.v5,
-          Text(
-            'You can always change this later',
-            style: TextStyle(
-              fontSize: Sizes.size16,
+                fontSize: Sizes.size16,
+              ),
             ),
-          ),
-          Gaps.v10,
-          TextField(
-            controller: usernameController,
-            cursorColor: Theme.of(context).primaryColor,
-            decoration: InputDecoration(
-                hintText: 'Username',
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
+            Gaps.v10,
+            TextField(
+              controller: usernameController,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: InputDecoration(
+                  errorText: isUsernameValid(),
+                  hintText: 'Username',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
-                  ),
-                )),
-          ),
-          Gaps.v20,
-          FormButton(
-            onTap: onEmailTap,
-            isDisable: username.isEmpty,
-          )
-        ]),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  )),
+            ),
+            Gaps.v20,
+            FormButton(
+              onTap: onEmailTap,
+              isDisable: username.isEmpty,
+            )
+          ]),
+        ),
       ),
     );
   }
