c31cbfbd811ffd9db9c9acd19c629b9d4e14fb27
parkjaehee <jaehee3505@naver.com>
2024-01-31 23:19:32 +0900

    Comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_app/constants/Gaps.dart';
+import 'package:tictok_app/constants/Sizes.dart';
 
 class VideoComments extends StatefulWidget {
   const VideoComments({super.key});
@@ -21,21 +23,75 @@ class _VideoCommentsState extends State<VideoComments> {
         borderRadius: BorderRadius.circular(25),
       ),
       child: Scaffold(
+        backgroundColor: Colors.grey.shade50,
+        appBar: AppBar(
           backgroundColor: Colors.grey.shade50,
-          appBar: AppBar(
-            backgroundColor: Colors.grey.shade50,
-            automaticallyImplyLeading: false,
-            actions: [
-              IconButton(
-                  onPressed: _onXmarkTap, icon: FaIcon(FontAwesomeIcons.xmark)),
+          automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: _onXmarkTap, icon: FaIcon(FontAwesomeIcons.xmark)),
+          ],
+          title: Text('22793 comments'),
+        ),
+        body: ListView.separated(
+          separatorBuilder: (context, index) => Gaps.v20,
+          padding: EdgeInsets.symmetric(
+              vertical: Sizes.size10, horizontal: Sizes.size16),
+          itemCount: 10,
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 18,
+                child: Text('네로'),
+              ),
+              Gaps.h10,
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('네로',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size14,
+                        color: Colors.grey.shade500,
+                      )),
+                  Gaps.v3,
+                  Text('sdfsdfdsfsdfsdfdsfsdfdsfdssdfsdfsdfsfsdfsf'),
+                ],
+              )),
+              Gaps.v10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v2,
+                  Text(
+                    '62.2k',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  )
+                ],
+              )
             ],
-            title: Text('22793 comments'),
           ),
-          body: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) => Container(
-                    child: Text('im a comment'),
-                  ))),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(children: [
+            CircleAvatar(
+              radius: 18,
+              child: Text('네로'),
+              backgroundColor: Colors.grey.shade500,
+              foregroundColor: Colors.white,
+            )
+          ]),
+        ),
+      ),
     );
   }
 }
