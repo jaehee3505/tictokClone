0be8de31ee833fb1ecb703b3d6951211bc118189
parkjaehee <jaehee3505@naver.com>
2024-01-31 16:01:45 +0900

    PageController

--- lib/features/videos/timeline_screen.dart
+++ lib/features/videos/timeline_screen.dart
@@ -12,7 +12,10 @@ class _TimelineScreenState extends State<TimelineScreen> {
   int _itemCount = 4;
   List<Color> colors = [Colors.blue, Colors.yellow, Colors.teal, Colors.amber];
 
+  final PageController _pageController = PageController();
   void _onPageChange(int index) {
+    _pageController.animateToPage(index,
+        duration: Duration(milliseconds: 150), curve: Curves.linear);
     if (index == _itemCount - 1) {
       _itemCount += 4;
       colors.addAll([Colors.blue, Colors.yellow, Colors.teal, Colors.amber]);
@@ -23,6 +26,7 @@ class _TimelineScreenState extends State<TimelineScreen> {
   @override
   Widget build(BuildContext context) {
     return PageView.builder(
+      controller: _pageController,
       onPageChanged: _onPageChange,
       scrollDirection: Axis.vertical,
       itemCount: _itemCount,
