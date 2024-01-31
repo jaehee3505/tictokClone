a953f6894f805d742470c88020d1e89e0df38da3
parkjaehee <jaehee3505@naver.com>
2024-01-31 17:58:43 +0900

    VisibilityDetector

--- lib/features/videos/timeline_screen.dart
+++ lib/features/videos/timeline_screen.dart
@@ -43,6 +43,6 @@ class _TimelineScreenState extends State<TimelineScreen> {
         scrollDirection: Axis.vertical,
         itemCount: _itemCount,
         itemBuilder: (context, index) =>
-            VideoPost(onVideoFinished: _onVideoFinished));
+            VideoPost(onVideoFinished: _onVideoFinished, index: index));
   }
 }
--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
-  const VideoPost({super.key, required this.onVideoFinished});
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+    required this.index,
+  });
 
   final Function onVideoFinished;
+  final int index;
   @override
   State<VideoPost> createState() => _VideoPostState();
 }
 
 class _VideoPostState extends State<VideoPost> {
-  VideoPlayerController _videoPlayerController =
+  final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset('assets/videos/videoSample01.mp4');
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
       if (_videoPlayerController.value.duration ==
           _videoPlayerController.value.position) {
-        print('비디오 끝남');
         widget.onVideoFinished();
       }
     }
@@ -25,7 +31,6 @@ class _VideoPostState extends State<VideoPost> {
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
     setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
   }
@@ -42,16 +47,46 @@ class _VideoPostState extends State<VideoPost> {
     super.dispose();
   }
 
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
+  void _onTapToggled() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+    } else {
+      _videoPlayerController.play();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: _videoPlayerController.value.isInitialized
-              ? VideoPlayer(_videoPlayerController)
-              : Container(color: Colors.teal),
-        )
-      ],
+    return VisibilityDetector(
+      key: Key('${widget.index}'),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(color: Colors.teal),
+          ),
+          Positioned.fill(
+              child: GestureDetector(
+            onTap: _onTapToggled,
+          )),
+          Positioned.fill(
+              child: IgnorePointer(
+            child: Center(
+                child: FaIcon(
+              FontAwesomeIcons.play,
+              color: Colors.white,
+            )),
+          )),
+        ],
+      ),
     );
   }
 }
