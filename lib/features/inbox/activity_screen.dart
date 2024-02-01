51e9c05e2cfd77fd050ec099761a83c63133158c
parkjaehee <jaehee3505@naver.com>
2024-02-01 15:27:29 +0900

    Dismissible

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -10,60 +10,91 @@ class ActivityScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('All activity')),
-      body: ListView(
-          padding: EdgeInsets.symmetric(
-            horizontal: Sizes.size20,
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
           ),
-          children: [
-            Text(
-              'New',
-              style: TextStyle(
-                  fontSize: Sizes.size14, color: Colors.grey.shade400),
+          child: Text(
+            'New',
+            style:
+                TextStyle(fontSize: Sizes.size14, color: Colors.grey.shade400),
+          ),
+        ),
+        Gaps.v14,
+        Dismissible(
+          background: Container(
+            alignment: Alignment.centerLeft,
+            color: Colors.green,
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: Sizes.size10,
+              ),
+              child: FaIcon(
+                FontAwesomeIcons.checkDouble,
+                color: Colors.white,
+                size: Sizes.size32,
+              ),
             ),
-            Gaps.v14,
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              leading: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey.shade400,
-                    width: Sizes.size1,
-                  ),
-                  shape: BoxShape.circle,
-                  color: Colors.white,
+          ),
+          secondaryBackground: Container(
+            alignment: Alignment.centerRight,
+            color: Colors.red,
+            child: Padding(
+              padding: EdgeInsets.only(
+                right: Sizes.size10,
+              ),
+              child: FaIcon(
+                FontAwesomeIcons.trash,
+                color: Colors.white,
+                size: Sizes.size32,
+              ),
+            ),
+          ),
+          key: Key('x'),
+          child: ListTile(
+            leading: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey.shade400,
+                  width: Sizes.size1,
                 ),
-                width: Sizes.size52,
-                child: Center(
-                    child: FaIcon(
-                  FontAwesomeIcons.bell,
-                  color: Colors.black,
-                )),
+                shape: BoxShape.circle,
+                color: Colors.white,
               ),
-              title: RichText(
-                text: TextSpan(
-                    text: 'Account updates',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
-                    ),
-                    children: [
-                      TextSpan(
-                          text: 'sub title sdfsdfdsfsdfds',
-                          style: TextStyle(fontWeight: FontWeight.normal)),
-                      TextSpan(
-                          text: '1hour',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.grey,
-                          ))
-                    ]),
-              ),
-              trailing: FaIcon(
-                FontAwesomeIcons.chevronRight,
-                size: Sizes.size16,
-              ),
-            )
-          ]),
+              width: Sizes.size52,
+              child: Center(
+                  child: FaIcon(
+                FontAwesomeIcons.bell,
+                color: Colors.black,
+              )),
+            ),
+            title: RichText(
+              text: TextSpan(
+                  text: 'Account updates',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                        text: 'sub title sdfsdfdsfsdfds',
+                        style: TextStyle(fontWeight: FontWeight.normal)),
+                    TextSpan(
+                        text: '1hour',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey,
+                        ))
+                  ]),
+            ),
+            trailing: FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size16,
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:tictok_app/constants/Sizes.dart';
+import 'package:tictok_app/features/inbox/activity_screen.dart';
 import 'package:tictok_app/features/main_navigation/main_navigation_screen.dart';
 import 'package:tictok_app/features/onboarding/interests_screen.dart';
 
@@ -16,31 +17,31 @@ class TicTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          splashColor: Colors.transparent, // 스플레시 효과 사라짐
-          // highlightColor: Colors.transparent, // 버튼 눌렀을때 물감혀과 사라짐
-          textSelectionTheme: TextSelectionThemeData(
-            cursorColor: Colors.black,
-            selectionColor: Colors.grey,
-          ),
-          appBarTheme: AppBarTheme(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.white,
-            centerTitle: true,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w600,
-              fontSize: Sizes.size16 + Sizes.size2,
+        theme: ThemeData(
+            splashColor: Colors.transparent, // 스플레시 효과 사라짐
+            // highlightColor: Colors.transparent, // 버튼 눌렀을때 물감혀과 사라짐
+            textSelectionTheme: TextSelectionThemeData(
+              cursorColor: Colors.black,
+              selectionColor: Colors.grey,
             ),
-          ),
-          bottomAppBarTheme: BottomAppBarTheme(
-            color: Colors.white,
-            elevation: 0,
-          ),
-          primaryColor: const Color(0xFFE9435A),
-          scaffoldBackgroundColor: Colors.white),
-      home: MainNavigationScreen(), //SignUpScreen(),
-    );
+            appBarTheme: AppBarTheme(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white,
+              centerTitle: true,
+              elevation: 0,
+              titleTextStyle: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16 + Sizes.size2,
+              ),
+            ),
+            bottomAppBarTheme: BottomAppBarTheme(
+              color: Colors.white,
+              elevation: 0,
+            ),
+            primaryColor: const Color(0xFFE9435A),
+            scaffoldBackgroundColor: Colors.white),
+        home: ActivityScreen() //MainNavigationScreen(), //SignUpScreen(),
+        );
   }
 }
