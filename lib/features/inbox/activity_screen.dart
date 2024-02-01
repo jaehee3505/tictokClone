6532ef2f348e04d348fe318ae6d5d283228cc314
parkjaehee <jaehee3505@naver.com>
2024-02-01 15:49:32 +0900

    RotationTransition

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -10,17 +10,53 @@ class ActivityScreen extends StatefulWidget {
   State<ActivityScreen> createState() => _ActivityScreenState();
 }
 
-class _ActivityScreenState extends State<ActivityScreen> {
+class _ActivityScreenState extends State<ActivityScreen>
+    with SingleTickerProviderStateMixin {
   final List<String> _notifications = List.generate(20, (index) => '${index}h');
 
   void _onDismissed(String notification) {
     _notifications.remove(notification);
   }
 
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 300),
+  );
+
+  late final Animation<double> _animation = Tween(
+    begin: 0.0,
+    end: 0.5,
+  ).animate(_animationController);
+
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('All activity')),
+      appBar: AppBar(
+          title: GestureDetector(
+        onTap: _onTitleTap,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('All activity'),
+            Gaps.h2,
+            RotationTransition(
+              turns: _animation,
+              child: FaIcon(
+                FontAwesomeIcons.chevronDown,
+                size: Sizes.size14,
+              ),
+            )
+          ],
+        ),
+      )),
       body: ListView(children: [
         Padding(
           padding: const EdgeInsets.symmetric(
