1aad945b9947f847edd53d1b3776511a98824545
parkjaehee <jaehee3505@naver.com>
2024-02-03 23:30:24 +0900

    animatedList part two

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tictok_app/constants/Sizes.dart';
+import 'package:tictok_app/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -14,11 +15,65 @@ class _ChatScreenState extends State<ChatsScreen> {
 
   List<int> _items = [];
 
+  final Duration _duration = Duration(milliseconds: 300);
+
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(_items.length);
+      _key.currentState!.insertItem(_items.length, duration: _duration);
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+          index,
+          (context, animation) => SizeTransition(
+              sizeFactor: animation,
+              child: Container(color: Colors.red, child: _makeTile(index))),
+          duration: _duration);
+      _items.remove(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: CircleAvatar(
+        radius: 25,
+        child: Text('네로'),
+        foregroundImage: NetworkImage(
+            'https://avatars.githubusercontent.com/u/68673384?s=40&v=4'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Lynn $index',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            '2:17pm',
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: Text('last messsage blablalbl'),
+    );
   }
 
   @override
@@ -45,36 +100,8 @@ class _ChatScreenState extends State<ChatsScreen> {
           return FadeTransition(
             key: UniqueKey(),
             opacity: animation,
-            child: SizeTransition(
-              sizeFactor: animation,
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 25,
-                  child: Text('네로'),
-                  foregroundImage: NetworkImage(
-                      'https://avatars.githubusercontent.com/u/68673384?s=40&v=4'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Lynn $index',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      '2:17pm',
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: Text('last messsage blablalbl'),
-              ),
-            ),
+            child:
+                SizeTransition(sizeFactor: animation, child: _makeTile(index)),
           );
         },
       ),
