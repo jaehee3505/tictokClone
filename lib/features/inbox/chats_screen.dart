08605320b1219faf58768f2a7bc3b3a5f286919e
parkjaehee <jaehee3505@naver.com>
2024-02-03 23:15:15 +0900

    AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -10,6 +10,17 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(_items.length);
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,46 +29,55 @@ class _ChatScreenState extends State<ChatsScreen> {
         elevation: 1,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: FaIcon(
               FontAwesomeIcons.plus,
             ),
           )
         ],
       ),
-      body: ListView(
-          padding: EdgeInsets.symmetric(
-            vertical: Sizes.size10,
-          ),
-          children: [
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25,
-                child: Text('네로'),
-                foregroundImage: NetworkImage(
-                    'https://avatars.githubusercontent.com/u/68673384?s=40&v=4'),
-              ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Lynn',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
+      body: AnimatedList(
+        padding: EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        key: _key,
+        itemBuilder: (context, index, Animation<double> animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 25,
+                  child: Text('네로'),
+                  foregroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/68673384?s=40&v=4'),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Lynn $index',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  ),
-                  Text(
-                    '2:17pm',
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontSize: Sizes.size12,
+                    Text(
+                      '2:17pm',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+                subtitle: Text('last messsage blablalbl'),
               ),
-              subtitle: Text('last messsage blablalbl'),
-            )
-          ]),
+            ),
+          );
+        },
+      ),
     );
   }
 }
