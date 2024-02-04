6ef31391127342c28cd1979cae1ecd688b05a1c6
parkjaehee <jaehee3505@naver.com>
2024-02-04 15:15:57 +0900

    aboutlisttile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -7,15 +7,25 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Settings'),
-        ),
-        body: Column(
-          children: [
-            CupertinoActivityIndicator(),
-            CircularProgressIndicator(),
-            CircularProgressIndicator.adaptive()
-          ],
-        ));
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => showAboutDialog(
+                context: context,
+                applicationVersion: '1.0',
+                applicationLegalese: 'all rightss'),
+            title: Text(
+              'aoube',
+              style: TextStyle(fontWeight: FontWeight.w600),
+            ),
+            subtitle: Text('about this app...'),
+          ),
+          AboutListTile(),
+        ],
+      ),
+    );
   }
 }
