14f59f205acf4f94eb68ac7ff1e74a6fb4de0af4
parkjaehee <jaehee3505@naver.com>
2024-02-04 12:27:30 +0900

    CustomScrollview

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -10,6 +10,40 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView();
+    return CustomScrollView(
+      physics: BouncingScrollPhysics(), //아래로 스크롤시 땡겨지는 것 추가
+      slivers: [
+        SliverAppBar(
+          //floating: true,
+          stretch: true,
+          pinned: true, //타이틀은 안사라지게 함
+          backgroundColor: Colors.teal,
+          elevation: 1,
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+              stretchModes: [
+                StretchMode.blurBackground,
+                StretchMode.fadeTitle,
+                StretchMode.zoomBackground,
+              ],
+              titlePadding:
+                  const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+              title: Text('hello'),
+              centerTitle: true,
+              background: Image.asset(
+                'assets/images/sample.png',
+                fit: BoxFit.cover,
+              )),
+        ),
+        SliverFixedExtentList(
+          // 위로 땡겼을때 줄어들게 해주는 것 추가
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => Container(),
+          ),
+          itemExtent: 1,
+        ),
+      ],
+    );
   }
 }
