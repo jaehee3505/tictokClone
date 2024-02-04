6b207dbf7486a24e10ecb581c9884b079343d749
parkjaehee <jaehee3505@naver.com>
2024-02-04 12:48:17 +0900

    slivergrid

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictok_app/constants/Sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -14,8 +15,6 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       physics: BouncingScrollPhysics(), //아래로 스크롤시 땡겨지는 것 추가
       slivers: [
         SliverAppBar(
-          snap: true,
-          floating: true,
           pinned: true,
           stretch: true,
           backgroundColor: Colors.teal,
@@ -23,19 +22,20 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           collapsedHeight: 80,
           expandedHeight: 200,
           flexibleSpace: FlexibleSpaceBar(
-              stretchModes: [
-                StretchMode.blurBackground,
-                StretchMode.zoomBackground,
-                StretchMode.fadeTitle,
-              ],
-              titlePadding:
-                  const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
-              title: Text('hello'),
-              centerTitle: true,
-              background: Image.asset(
-                'assets/images/sample.png',
-                fit: BoxFit.cover,
-              )),
+            stretchModes: [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
+            ],
+            titlePadding:
+                const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+            title: Text('hello'),
+            centerTitle: true,
+            background: Image.asset(
+              'assets/images/sample.png',
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
         SliverFixedExtentList(
           // 위로 땡겼을때 줄어들게 해주는 것 추가
@@ -44,11 +44,31 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             (context, index) => Container(
               color: Colors.amber[100 * (index % 9)],
               child: Align(
-                  alignment: Alignment.center, child: Text('item $index')),
+                alignment: Alignment.center,
+                child: Text('item $index'),
+              ),
             ),
           ),
           itemExtent: 100,
         ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.blue[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text('item $index'),
+              ),
+            ),
+          ),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100,
+            mainAxisSpacing: Sizes.size20,
+            crossAxisSpacing: Sizes.size20,
+            childAspectRatio: 1,
+          ),
+        )
       ],
     );
   }
